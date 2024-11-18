24d18c4330e7d32c9aba7661425c69592a6dbe97
HoovyBeast05 <hussein05.ae@alumnes.ilerna.com>
2024-11-18 19:46:48 +0100

    Introduction to the game
    Im just copy pasting shit I already done

--- src/juego/Juego_colgado.java
+++ src/juego/Juego_colgado.java
@@ -1,9 +1,18 @@
 package juego;
 
+import java.util.Scanner;
+
 public class Juego_colgado {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Welcome to the hanged man game!!");
+		System.out.println("In this game, you will have 6 tries to guess the word or DIE trying");
+		System.out.println("...");
+		System.out.println("Good luck!");
 
 	}
 
